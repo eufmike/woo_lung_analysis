6f1255c7b59f1b6ceb3023d7838fb07310b07f2b
Michael Shih <sc2.shih@gmail.com>
2018-05-04 15:48:03 -0500

    test

--- lung.py
+++ lung.py
@@ -23,7 +23,6 @@ combined_data_point = pd.DataFrame([])
 combined_data_segment = pd.DataFrame([])
 
 for m in filenames:
-    
     # create file path
     filepath = os.path.join(dir, datafolder, m)
     data = pd.ExcelFile(filepath)
@@ -57,24 +56,75 @@ reload(dp)
 Segment_ID, Node_ID_1, Node_ID_2, df = dp.combine_point_seg(combined_data_point, combined_data_segment)
 
 # display(combined_data_segment.head(5))
-'''
+
 print(Segment_ID)
 print(Node_ID_1)
 print(Node_ID_2)
-'''
+
 print(df)
 
 
 #%%
+df.to_csv("complited_data.csv", sep='\t')
+
+#%%
+df.to_pickle("complited_data.pkl")
+
+#%%
+store = pd.HDFStore('complited_data.h5')
+store['df'] = df
+
+
+
+#%%
+df = store['df']
+df
+#%%
+grouped_data = df.groupby([df['objectID'], df['Segment ID']])
+grouped_mean = grouped_data.mean()
+grouped_mean
+grouped_mean['objectID']
+grouped_data.groups.keys()
+
+#%%
+#df['thickness'].hist(by=df['objectID'], normed=1)
+grouped_mean['thickness'].hist()
+
+#%%
 import matplotlib.pyplot as plt
-import seaborn
-seaborn.set()
+import seaborn as sns
+sns.set()
+
+grouped_data.groups.keys()
+
+objectname = np.unique(grouped_mean['objectID'])
+print(objectname)
+normed_par = 1
+n_bins=range(0, 200, 2)
+
+for i in objectname:
+    plt.hist(i, n_bins, alpha=0.5, label = i, normed=normed_par)
+
+plt.xlim(left=0, right=200)
+plt.xlabel('thickness (µm)')
+plt.ylabel('Probability')
+plt.legend(loc='upper right')
+plt.show()
+
+
+
+
+
+
+#%%
 # n, bins, patches = plt.hist(data_point['thickness'], 50, normed=1, facecolor='green', alpha=0.75)
 # plt.xlabel('thickness (µm)')
 # plt.ylabel('Probability')
 # plt.show()
 # plt.savefig('thickness_histogram.png')
 objectname = np.unique(combined_data_point['objectID'])
+
+
 x1 = combined_data_point.loc[combined_data_point['objectID'] == objectname[0], 'thickness']
 x2 = combined_data_point.loc[combined_data_point['objectID'] == objectname[1], 'thickness']
 n_bins=range(0, 200, 2)
